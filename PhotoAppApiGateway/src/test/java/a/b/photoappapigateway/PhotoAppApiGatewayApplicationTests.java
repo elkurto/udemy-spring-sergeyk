caa0b162336c5c166b8c676458c0e1dc99f492ad
elkurto <kurt.buehner@gmail.com>
2025-06-13 16:03:30 -0400

    added api-gateway

---
+++ PhotoAppApiGateway/src/test/java/a/b/photoappapigateway/PhotoAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package a.b.photoappapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

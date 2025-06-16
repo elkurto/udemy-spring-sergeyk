f2c11530b879e8ad5e72fbc9e1499fd3d68d3bdc
elkurto <kurt.buehner@gmail.com>
2025-06-16 10:02:23 -0400

    mod UsersController.java

--- PhotoAppApiUsers/src/main/java/a/b/photoapp/api/users/rest/UsersController.java
+++ PhotoAppApiUsers/src/main/java/a/b/photoapp/api/users/rest/UsersController.java
@@ -1,5 +1,7 @@
 package a.b.photoapp.api.users.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+  @Autowired
+  Environment environment;
+
   @GetMapping("")
   public String index() {
     return "pong";
   }
   @GetMapping("/status/check")
   public String status() {
-    return "working";
+    return "working on port ="+ environment.getProperty("local.server.port");
   }
   @GetMapping("/hello")
   public String hello() {
